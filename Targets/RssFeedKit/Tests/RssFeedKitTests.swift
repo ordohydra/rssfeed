cab2233bab3dec4b8ef4df91676f2b7ceb90f354
Виктор Сукочев <vsukochev@yandex.ru>
2024-09-28 17:09:44 +0300

    Tuist initial project

---
+++ Targets/RssFeedKit/Tests/RssFeedKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class RssFeedKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("RssFeedKit", "RssFeedKit")
+    }
+}
---
+++ Targets/RssFeedUI/Tests/RssFeedUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class RssFeedUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("RssFeedUI", "RssFeedUI")
+    }
+}
